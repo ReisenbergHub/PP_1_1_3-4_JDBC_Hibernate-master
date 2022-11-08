dfadb7395174d7c208c9d2834ccd180b354defd4
Reisenberg <yakovlev.a.official@yandex.ru>
2022-11-08 20:31:47 +0500

    add Unversioned Files (50 files)

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,140 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.connection;
+
+public class UserDaoJDBCImpl implements UserDao {
+    public UserDaoJDBCImpl() {
+
+    }
+
+    public void createUsersTable() {
+        try {
+            Statement statement = connection.createStatement();
+            connection.setAutoCommit(false);
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS User (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), last_name VARCHAR(45), age INT);");
+            statement.close();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void dropUsersTable() {
+        try {
+            Statement statement = connection.createStatement();
+            connection.setAutoCommit(false);
+            statement.executeUpdate(" DROP TABLE IF EXISTS User");
+            statement.close();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO User (name, last_name, age) VALUES (?, ?, ?)");
+            connection.setAutoCommit(false);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,lastName);
+            preparedStatement.setByte(3,age);
+            preparedStatement.executeUpdate();
+            connection.commit();
+            preparedStatement.close();
+            System.out.println("User " + name + " added in database");
+
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void removeUserById(long id) {
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM User WHERE id=?");
+            preparedStatement.setLong(1,id);
+            preparedStatement.executeUpdate();
+            connection.commit();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            connection.setAutoCommit(false);
+            String SQL = "SELECT * From User";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setAge(resultSet.getByte("age"));
+                people.add(user);
+            }
+            statement.close();
+        } catch (Exception e) {
+            try {
+                connection.rollback();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+        return people;
+    }
+
+    public void cleanUsersTable() {
+        try{
+            Statement statement = connection.createStatement();
+            connection.setAutoCommit(false);
+            statement.executeUpdate("TRUNCATE User");
+            System.out.println("Database clear");
+            connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+}
